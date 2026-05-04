5051b58a73bb6fa207f8f45aeca52dd1d27c583c
rizky24si <rizky24si@mahasiswa.pcr.ac.id>
2026-05-04 22:18:57 +0700

    Update Project CRM V3

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -8,15 +8,9 @@ const MainLayout = () => {
 
   return (
     <div className="flex h-screen bg-gray-50">
-      {/* Sidebar di kiri */}
       <Sidebar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
-      
-      {/* Area konten kanan - ini yang harusnya sejajar dengan sidebar */}
       <div className="flex-1 flex flex-col">
-        {/* Header di atas area konten */}
         <Header isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
-        
-        {/* Main content - ini yang tidak boleh turun ke bawah */}
         <main className="flex-1 overflow-y-auto p-6 lg:p-8">
           <div className="max-w-7xl mx-auto">
             <Outlet />
